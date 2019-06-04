e875aa2ab9e38b0bb1288fda46ee2a99e239f5cb
Arpan Patel <adp8ke@gmail.com>
2019-06-04 11:39:33 -0400

    max and min using while

---
+++ max_min.js
@@ -0,0 +1,31 @@
+function findMax(numbers){
+    let max= numbers[0];
+
+    let  i = 1;
+
+    while (i < numbers.length){
+        if (numbers[i]>max){
+            max = numbers[i];
+            i++;
+        }
+    }
+    return max;
+}
+
+function findMin(numbers){
+    let min= numbers[0];
+
+    let  i = 1;
+
+    while (i < numbers.length){
+        if (numbers[i]<min){
+            min = numbers[i];
+        }
+        i++;
+    }
+    return min;
+}
+
+numbers = [1, 2, 3, 4, 5]
+console.log(findMax(numbers));
+console.log(findMin(numbers));
